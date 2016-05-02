bc3deec04630b0bddf23c9890a1dc1084549ea9f
Philipp Moritz <pcmoritz@gmail.com>
2016-05-02 09:37:08 -0700

    reduce logging further

--- lib/orchpy/orchpy/worker.py
+++ lib/orchpy/orchpy/worker.py
@@ -115,10 +115,10 @@ def distributed(arg_types, return_types, worker=global_worker):
   def distributed_decorator(func):
     def func_executor(arguments):
       """This is what gets executed remotely on a worker after a distributed function is scheduled by the scheduler."""
-      print "Calling function {}".format(func.__name__)
+      # print "Calling function {}".format(func.__name__)
       result = func(*arguments)
       check_return_values(func_call, result) # throws an exception if result is invalid
-      print "Finished executing function {}".format(func.__name__)
+      # print "Finished executing function {}".format(func.__name__)
       return result
     def func_call(*args):
       """This is what gets run immediately when a worker calls a distributed function."""
