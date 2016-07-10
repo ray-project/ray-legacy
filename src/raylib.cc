4f93bdb4723a336a86bc68dfd87fc1b9375b44ef
Philipp Moritz <pcmoritz@gmail.com>
2016-07-09 21:32:58 -0700

    expose available workers

--- src/raylib.cc
+++ src/raylib.cc
@@ -815,11 +815,16 @@ static PyObject* scheduler_info(PyObject* self, PyObject* args) {
   for (size_t i = 0; i < reply.task_queue_size(); ++i) {
     PyList_SetItem(task_queue_list, i, PyInt_FromLong(reply.task_queue(i)));
   }
+  PyObject* avail_workers_list = PyList_New(reply.avail_worker_size());
+  for (size_t i = 0; i < reply.avail_worker_size(); ++i) {
+    PyList_SetItem(task_queue_list, i, PyInt_FromLong(reply.avail_worker(i)));
+  }
 
   PyObject* dict = PyDict_New();
   set_dict_item_and_transfer_ownership(dict, PyString_FromString("target_objrefs"), target_objref_list);
   set_dict_item_and_transfer_ownership(dict, PyString_FromString("reference_counts"), reference_count_list);
   set_dict_item_and_transfer_ownership(dict, PyString_FromString("task_queue"), task_queue_list);
+  set_dict_item_and_transfer_ownership(dict, PyString_FromString("avail_workers"), avail_workers_list);
 
   return dict;
 }
