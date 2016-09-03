9ea4c35f925b82ad986c977552ad07a3c8ece18b
Robert Nishihara <robertnishihara@gmail.com>
2016-09-03 12:48:54 -0700

    Throw a fatal error if a gRPC call fails.

--- include/ray/logging.h
+++ include/ray/logging.h
@@ -61,8 +61,13 @@ extern "C" __declspec(dllimport) int __stdcall IsDebuggerPresent();
 #define RAY_CHECK_GE(var1, var2, message) RAY_CHECK((var1) >= (var2), message)
 #define RAY_CHECK_GT(var1, var2, message) RAY_CHECK((var1) > (var2), message)
 
-#define RAY_CHECK_GRPC(expr) \
+#define RAY_WARN_GRPC(expr) \
   do { \
     grpc::Status _s = (expr); \
     RAY_WARN(_s.ok(), "grpc call failed with message " << _s.error_message()); \
+  } while (0);
+#define RAY_CHECK_GRPC(expr) \
+  do { \
+    grpc::Status _s = (expr); \
+    RAY_CHECK(_s.ok(), "grpc call failed with message " << _s.error_message()); \
   } while (0);
